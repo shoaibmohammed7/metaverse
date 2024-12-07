1e20778d47b27d3f759ea0b55279c16f28b838a1
Shoaib Mohammed <smohammed7@hawk.iit.edu>
2024-12-07 13:30:57 -0600

    initial commit

---
+++ orbitspace/apps/http/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { router } from "./routes/v1";
+import client from "@repo/db"
+
+const app = express()
+
+app.use(express.json())
+
+app.use("/api/v1",router)
+
+
+app.listen(process.env.PORT || 3000)
